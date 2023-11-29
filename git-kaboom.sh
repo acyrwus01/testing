c10f031b9fb748c6a7c40feaf1762abf3689580f
acyrwus01 <acyrwus01@gmail.com>
2023-11-28 21:07:04 -0700

    update kaboom

--- git-kaboom.sh
+++ git-kaboom.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-# Usage: ./script.sh <commit-hash>
-
 # Exit if any command fails
 set -e
 
-# Check if commit hash is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <commit-hash>"
-    exit 1
-fi
-
-COMMIT_HASH=$1
-
 # Function to deinitialize unneeded submodules
 deinit_unneeded_submodules() {
-    # Get list of submodules at the specific commit
-    local submodules_at_commit=$(git ls-tree -r $COMMIT_HASH | grep '^160000' | awk '{print $4}')
+    # Get list of submodules at the head of the branch
+    local submodules_at_branch=$(git ls-tree -r $BRANCH_NAME | grep '^160000' | awk '{print $4}')
 
     # Get list of currently initialized submodules
     local current_submodules=$(git submodule status | awk '{print $2}')
 
     for submodule in $current_submodules; do
-        if ! echo $submodules_at_commit | grep -q "$submodule"; then
+        if ! echo $submodules_at_branch | grep -q "$submodule"; then
             echo "Deinitializing unneeded submodule: $submodule"
             git submodule deinit -f "$submodule"
         fi
     done
 }
 
-# Checkout the specific commit
-git checkout $COMMIT_HASH
+# List all branches and use gum to select one
+BRANCH_NAME=$(git branch -a | grep -v 'remotes' | sed 's/*//' | sed 's/ //' | gum choose)
+
+if [ -z "$BRANCH_NAME" ]; then
+    echo "No branch selected, exiting."
+    exit 1
+fi
+
+# Checkout the selected branch
+git checkout $BRANCH_NAME
 
 # Update and initialize submodules
 git submodule update --init --recursive
@@ -45,5 +43,5 @@ git submodule foreach --recursive git reset --hard
 # Deinitialize unneeded submodules
 deinit_unneeded_submodules
 
-echo "Repository and submodules reset to commit $COMMIT_HASH."
+echo "Repository and submodules reset to the latest commit of branch $BRANCH_NAME."
 
